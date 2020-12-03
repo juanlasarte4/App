d779e51b630c434d139bf26fe8abc7f2d25bcb6b
juanlasarte4 <0302jlasarte@e-itaca.es>
2020-12-03 11:32:56 +0100

    Cliente y pedido

---
+++ src/main/java/Modelos/Cliente.java
@@ -0,0 +1,102 @@
+package Modelos;
+
+public class Cliente {
+	private int codigoCliente;
+	private String nombreCliente;
+	private String contactoCliente;
+	private String apellidoCLiente;
+	private String telefonoCliente;
+	private String faxCliente;
+	private String direccionCliente;
+	private String direccion2Cliente;
+	private String ciudadCliente;
+	private String regionCliente;
+	private String paisCliente;
+	private String codigoPostalCliente;
+	private int codigoEmpleadoRepVentas;
+	private double limiteCredito;
+
+	public Cliente(int codigoCliente, String nombreCliente, String apellidoCLiente) {
+		super();
+		this.codigoCliente = codigoCliente;
+		this.nombreCliente = nombreCliente;
+		this.apellidoCLiente = apellidoCLiente;
+	}
+
+	public Cliente(int codigoCliente, String nombreCliente, String contactoCliente, String apellidoCLiente,
+			String telefonoCliente, String faxCliente, String direccionCliente, String direccion2Cliente,
+			String ciudadCliente, String regionCliente, String paisCliente, String codigoPostalCliente,
+			int codigoEmpleadoRepVentas, double limiteCredito) {
+		super();
+		this.codigoCliente = codigoCliente;
+		this.nombreCliente = nombreCliente;
+		this.contactoCliente = contactoCliente;
+		this.apellidoCLiente = apellidoCLiente;
+		this.telefonoCliente = telefonoCliente;
+		this.faxCliente = faxCliente;
+		this.direccionCliente = direccionCliente;
+		this.direccion2Cliente = direccion2Cliente;
+		this.ciudadCliente = ciudadCliente;
+		this.regionCliente = regionCliente;
+		this.paisCliente = paisCliente;
+		this.codigoPostalCliente = codigoPostalCliente;
+		this.codigoEmpleadoRepVentas = codigoEmpleadoRepVentas;
+		this.limiteCredito = limiteCredito;
+	}
+
+	public int getCodigoCliente() {
+		return codigoCliente;
+	}
+
+	public String getNombreCliente() {
+		return nombreCliente;
+	}
+
+	public String getContactoCliente() {
+		return contactoCliente;
+	}
+
+	public String getApellidoCLiente() {
+		return apellidoCLiente;
+	}
+
+	public String getTelefonoCliente() {
+		return telefonoCliente;
+	}
+
+	public String getFaxCliente() {
+		return faxCliente;
+	}
+
+	public String getDireccionCliente() {
+		return direccionCliente;
+	}
+
+	public String getDireccion2Cliente() {
+		return direccion2Cliente;
+	}
+
+	public String getCiudadCliente() {
+		return ciudadCliente;
+	}
+
+	public String getRegionCliente() {
+		return regionCliente;
+	}
+
+	public String getPaisCliente() {
+		return paisCliente;
+	}
+
+	public String getCodigoPostalCliente() {
+		return codigoPostalCliente;
+	}
+
+	public int getCodigoEmpleadoRepVentas() {
+		return codigoEmpleadoRepVentas;
+	}
+
+	public double getLimiteCredito() {
+		return limiteCredito;
+	}
+}
---
+++ src/main/java/Modelos/Pedido.java
@@ -0,0 +1,81 @@
+package Modelos;
+
+import java.util.Calendar;
+
+public class Pedido {
+	private int codigoPedido;
+	private Calendar fechaPedido;
+	private Calendar fechaEsperada;
+	private Calendar fechaEntrega;
+	private String estadoPedido;
+	private String comentarios;
+	private int codigo_cliente;
+
+	public Pedido(int codigoPedido, Calendar fechaPedido, Calendar fechaEsperada, Calendar fechaEntrega, String estadoPedido,
+			String comentarios, int codigo_cliente) {
+		super();
+		this.codigoPedido = codigoPedido;
+		this.fechaPedido = fechaPedido;
+		this.fechaEsperada = fechaEsperada;
+		this.fechaEntrega = fechaEntrega;
+		this.estadoPedido = estadoPedido;
+		this.comentarios = comentarios;
+		this.codigo_cliente = codigo_cliente;
+	}
+
+	public int getCodigoPedido() {
+		return codigoPedido;
+	}
+
+	public void setCodigoPedido(int codigoPedido) {
+		this.codigoPedido = codigoPedido;
+	}
+
+	public Calendar getFechaPedido() {
+		return fechaPedido;
+	}
+
+	public void setFechaPedido(Calendar fechaPedido) {
+		this.fechaPedido = fechaPedido;
+	}
+
+	public Calendar getFechaEsperada() {
+		return fechaEsperada;
+	}
+
+	public void setFechaEsperada(Calendar fechaEsperada) {
+		this.fechaEsperada = fechaEsperada;
+	}
+
+	public Calendar getFechaEntrega() {
+		return fechaEntrega;
+	}
+
+	public void setFechaEntrega(Calendar fechaEntrega) {
+		this.fechaEntrega = fechaEntrega;
+	}
+
+	public String getEstadoPedido() {
+		return estadoPedido;
+	}
+
+	public void setEstadoPedido(String estadoPedido) {
+		this.estadoPedido = estadoPedido;
+	}
+
+	public String getComentarios() {
+		return comentarios;
+	}
+
+	public void setComentarios(String comentarios) {
+		this.comentarios = comentarios;
+	}
+
+	public int getCodigo_cliente() {
+		return codigo_cliente;
+	}
+
+	public void setCodigo_cliente(int codigo_cliente) {
+		this.codigo_cliente = codigo_cliente;
+	}
+}
